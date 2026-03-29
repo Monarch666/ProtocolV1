02612739f420a8ff07b26ce5574153b651ef99e5
I-am-Krish <krishnashisdas7@gmail.com>
2026-03-29 11:46:01 +0530

    chore: remove tracked editor scripts now in gitignore

--- seq_editor.py
+++
@@ -1,7 +0,0 @@
-
-import sys
-content = open(sys.argv[1]).read()
-# Replace the current hash 'acc6eba' with 'reword'
-new_content = content.replace('pick acc6eba', 'reword acc6eba')
-with open(sys.argv[1], 'w') as f2:
-    f2.write(new_content)
